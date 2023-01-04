4af88e90dcab88a4dba9e493475c5852fd3d5926
Calathea-Z <zsykes21@gmail.com>
2023-01-04 16:36:34 -0500

    Continue work on main feed. Add location, like and comment button.

--- ShoeString-frontend/src/Components/Feed.jsx
+++ ShoeString-frontend/src/Components/Feed.jsx
@@ -8,6 +8,7 @@ const fakePost = [
         username: "Mary Traveler",
         userphoto: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.explicit.bing.net%2Fth%3Fid%3DOIP.ZJOQOm4SvCRpk0Z6nyYNAgHaH0%26pid%3DApi&f=1&ipt=c1d0757557bd6cc009cf2fa8dad09994e3183b0780341e42ab991a46f26e739a&ipo=images",
         img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.pMbRD3YNuuS8Y98caOyCqgHaE7%26pid%3DApi&f=1&ipt=c08ec5abd60bb027601c7843e741d89861bd430651ee2c65b48336dc71718adc&ipo=images",
+        location: "London, England",
         comment: "I got to these cool mountains. To get there you have to turn left at THAT tree not THIS tree... got it?"
     },
     {
@@ -15,6 +16,7 @@ const fakePost = [
         username: "Mary Traveler",
         userphoto: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.explicit.bing.net%2Fth%3Fid%3DOIP.ZJOQOm4SvCRpk0Z6nyYNAgHaH0%26pid%3DApi&f=1&ipt=c1d0757557bd6cc009cf2fa8dad09994e3183b0780341e42ab991a46f26e739a&ipo=images",
         img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.pMbRD3YNuuS8Y98caOyCqgHaE7%26pid%3DApi&f=1&ipt=c08ec5abd60bb027601c7843e741d89861bd430651ee2c65b48336dc71718adc&ipo=images",
+        location: "London, England",
         comment: "I got to these cool mountains. To get there you have to turn left at THAT tree not THIS tree... got it?"
     },
     {
@@ -22,6 +24,7 @@ const fakePost = [
         username: "Mary Traveler",
         userphoto: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.explicit.bing.net%2Fth%3Fid%3DOIP.ZJOQOm4SvCRpk0Z6nyYNAgHaH0%26pid%3DApi&f=1&ipt=c1d0757557bd6cc009cf2fa8dad09994e3183b0780341e42ab991a46f26e739a&ipo=images",
         img: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.pMbRD3YNuuS8Y98caOyCqgHaE7%26pid%3DApi&f=1&ipt=c08ec5abd60bb027601c7843e741d89861bd430651ee2c65b48336dc71718adc&ipo=images",
+        location: "London, England",
         comment: "I got to these cool mountains. To get there you have to turn left at THAT tree not THIS tree... got it?"
     },
 ]
@@ -31,7 +34,7 @@ function Feed() {
   return (
     <div className='feed-full'>
         {fakePost.map((post) => (
-            <PostIndividual key={post.id} id={post.id} username={post.username} userphoto={post.userphoto} img={post.img} comment={post.comment} />
+            <PostIndividual key={post.id} id={post.id} username={post.username} userphoto={post.userphoto} img={post.img} location={post.location} comment={post.comment} />
       ))}
     </div>
   )
--- ShoeString-frontend/src/Components/PostIndividual.jsx
+++ ShoeString-frontend/src/Components/PostIndividual.jsx
@@ -1,18 +1,32 @@
 import '../Styles/postIndividual.css';
+import {ImHeart} from 'react-icons/im'
+import {FiMapPin} from 'react-icons/fi'
+import {BsFillChatSquareTextFill} from 'react-icons/bs'
 
-function PostIndividual({id_, username, userphoto, img, comment}) {
+function PostIndividual({id_, username, userphoto, img, location, comment}) {
   return (
     <div className='post-individual-full'>
+
       <div className='post-individual-header'>
         <p>{username}</p>
-        <img className='profile-photo' src={userphoto} alt=''/>
+        <img className='profile-photo' src={userphoto} alt={username}/>
       </div>
-      <div className='post-individual-photo'>
-        <img className='post-photo' src={img} alt=''/>
-      </div>
-      <div className='post-individual-comment'>
-        <h6>{comment}</h6>
-      </div>
+
+      <div className='post-body'>
+        <img className='post-photo' src={img} alt='Photo of location'/>
+       </div> 
+        <div className='post-individual-middle'>
+            <div>
+              <a href='#locationtag' className='location-button'><FiMapPin/>{' '}{location}</a>
+            </div>
+            <div>
+              <button className='post-button'><ImHeart/></button>
+              <button className='post-button'><BsFillChatSquareTextFill/></button>
+            </div>
+        </div>
+        <div className='post-individual-comment'>
+          <h6>{comment}</h6>
+        </div>
     </div>
   )
 }
