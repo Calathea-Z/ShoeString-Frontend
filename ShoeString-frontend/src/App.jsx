2828f5bf6df02bd8c48313f2de0b50039c110ad8
Calathea-Z <zsykes21@gmail.com>
2023-01-04 10:00:56 -0500

    Add react-icons dependency

--- ShoeString-frontend/src/App.jsx
+++ ShoeString-frontend/src/App.jsx
@@ -1,9 +1,9 @@
-import '../Styles/App.css';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div className="App">
-    <h1>Sanity</h1>
+    <NavBar />
     </div>
   );
 }
---
+++ ShoeString-frontend/src/Components/NavBar.jsx
@@ -0,0 +1,12 @@
+import '../Styles/navbar.css';
+
+const Nav = () => {
+  return (
+    <nav>
+      <a href='#'>Home</a>
+      <a href='#settings'>Settings</a>
+    </nav>
+  )
+}
+
+export default Nav
