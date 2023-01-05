2884221b385ad13b95213cc2e4429f30d3a8d877
Calathea-Z <zsykes21@gmail.com>
2023-01-05 08:34:17 -0500

    Updated dummy data for testing.

--- ShoeString-frontend/src/Pages/NotFound.jsx
+++ ShoeString-frontend/src/Pages/NotFound.jsx
@@ -3,7 +3,7 @@ import '../Styles/notFound.css'
 function NotFound() {
   return (
     <div className='not-found'>
-      <h1> 404: This information does not, did not, and may not ever exist? ...akward</h1>
+      <h1> 404: This information does not, did not, and may not ever exist ...akward</h1>
     </div>
   )
 }
